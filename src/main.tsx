8e962feff26d08bc31fec4285484bbda8455e2fe
Edutesc <luciano.puzer@ufabc.edu.br>
2025-08-12 15:53:51 -0300

    Adding queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import TestApi from "./pages/TestApi";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryclient";
+import AuthProvider from "./auth/AuthProvider";
+import { router } from "./routes/AppRoutes";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
- (<TestApi />)
-)
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
 
 
